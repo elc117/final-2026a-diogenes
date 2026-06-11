54ef4f43e441e66e50f1643eb1efc9a1dd8a237a
Di889 <88069861+Di889@users.noreply.github.com>
2026-06-11 18:58:54 -0300

    feat(battle): initialize units and BattleManager in BattleScreen

--- core/src/main/java/com/diogenes/wolfpack/screens/BattleScreen.java
+++ core/src/main/java/com/diogenes/wolfpack/screens/BattleScreen.java
@@ -1,13 +1,24 @@
 package com.diogenes.wolfpack.screens;
 
+
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
+
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.diogenes.wolfpack.WolfPack;
+import com.diogenes.wolfpack.battle.BattleManager;
+import com.diogenes.wolfpack.entities.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BattleScreen implements Screen {
 
     final WolfPack game;
+    private BattleManager battleManager;
+    private List<Wolf> wolves;
+    private List<Enemy> enemies;
 
     public BattleScreen(final WolfPack game) {
         this.game = game;
@@ -15,12 +26,21 @@ public class BattleScreen implements Screen {
 
     @Override
     public void show() {
+        initUnits();
+
+        battleManager = new BattleManager(wolves, enemies);
 
     }
 
     @Override
     public void render(float v) {
         ScreenUtils.clear(Color.BLACK);
+
+        game.batch.begin();
+
+        game.font.draw(game.batch, "placeholder", 100, 100);
+
+        game.batch.end();
     }
 
     @Override
@@ -45,6 +65,16 @@ public class BattleScreen implements Screen {
 
     @Override
     public void dispose() {
+    }
 
+    // test setup
+    public void initUnits(){
+        wolves = new ArrayList<>();
+        enemies = new ArrayList<>();
+
+        wolves.add(new Alpha());
+        wolves.add(new Healer());
+        wolves.add(new Scout());
+        enemies.add(new Boar());
     }
 }
