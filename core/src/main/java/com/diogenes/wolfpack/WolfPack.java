a4913b830eaecf10c2734d75c81c53125470432c
Di889 <88069861+Di889@users.noreply.github.com>
2026-06-11 18:57:44 -0300

    refactor(core): move SpriteBatch and BitmapFont to main WolfPack class

--- core/src/main/java/com/diogenes/wolfpack/WolfPack.java
+++ core/src/main/java/com/diogenes/wolfpack/WolfPack.java
@@ -1,12 +1,20 @@
 package com.diogenes.wolfpack;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.diogenes.wolfpack.screens.BattleScreen;
 
 public class WolfPack extends Game {
 
+    public SpriteBatch batch;
+    public BitmapFont font;
+
     @Override
     public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+
         this.setScreen(new BattleScreen(this));
     }
 
@@ -17,6 +25,7 @@ public class WolfPack extends Game {
 
     @Override
     public void dispose() {
-
+        batch.dispose();
+        font.dispose();
     }
 }
